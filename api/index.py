724ffd7fb210d8e4743288af277e33916fa828e2
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-24 20:53:07 +0100

    I'm tired

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
+from pymongo import MongoClient
 import os
 
 load_dotenv()
@@ -10,18 +11,46 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
-DB_NAME = os.getenv('DB_NAME')
+
+uri = os.getenv("MONGO_URI")
+db_name = os.getenv('DB_NAME')
+
+client = MongoClient(uri)
+db = client['Notes']
+
+Notes_db = db['Notes']
+
+class Notesdb:
+    def __init__(self) -> None:
+        self.collection = Notes_db
+        
+    def get_access_codes(self):
+        return self.collection.distinct("access_codes")
+    
+    def get_all_notes(self, query):
+        return self.collection.find_one(query)
+    
+    def find(self, query, key):
+        return self.collection.find_one(query, {key: 1, "_id": 0})
+    
+    def update_old(self, query, key, newNotes):
+        return self.collection.update_one(
+            query,
+            {"$push": {key: {"$each": newNotes}}}
+        )
+        
+    def create_new(self, query, key, newNotes):
+        return self.collection.update_one(
+            query,
+            {"$set": {key: newNotes}},
+            upsert=True  
+        )
+        
+    def delete_note(self, query, key, notes_to_delete):
+        return self.collection.update_one(query, {"$pull": {key: notes_to_delete[0]}})
 
 try:
-    mongo = PyMongo(app)
-    mongo.db.command('ping')
-    print("Connected to MongoDB!")
-except Exception as e:
-    print(f"Error connecting to MongoDB: {e}")
-    raise
-
-try:
-    Notes = mongo.db.notes
+    Notes = Notesdb()
 except Exception as e:
     print(f"Error accessing notes collection: {e}")
     raise
@@ -29,13 +58,13 @@ except Exception as e:
 
 @app.get("/api/get_all_access_codes")
 def get_all_access_codes():
-    all_access_codes = Notes.distinct("access_codes")
+    all_access_codes = Notes.get_access_codes()
     return jsonify({"message": "success", "access_codes": all_access_codes})
 
 @app.get('/api/confirmCode')
 def confirmCode():
     code = request.json.get('code')
-    if code not in mongo.db.notes.distinct('access_code'):
+    if code not in Notes.get_access_codes('access_code'):
         return jsonify({"error": "Invalid access code. Try again or generate a new one. You will lose all previous notes"}), 401
     return jsonify({"message": "Success"})
     
@@ -52,20 +81,13 @@ def save_notes():
     
     query = {"access_code": access_code}
     key = f'day_{day}.section_{section}'
-    document = Notes.find_one(query, {key: 1, "_id": 0})
+    document = Notes.find(query, key)
     
     if document and f'day_{day}' in document and f'section_{section}' in document[f'day_{day}']:
-        Notes.update_one(
-            query,
-            {"$push": {key: {"$each": newNotes}}}
-        )
+        Notes.update_old(query,key, newNotes)
         
     else: 
-        Notes.update_one(
-            query,
-            {"$set": {key: newNotes}},
-            upsert=True  
-        )
+        Notes.create_new(query,key, newNotes)
     return jsonify({'message': 'Notes saved successfully'}), 200
 
 @app.delete("/api/delete_note")
@@ -78,10 +100,9 @@ def delete_note():
     print(access_code, day, section, notes_to_delete)
     
     key = f'day_{day}.section_{section}'
-    print
     query = {"access_code": access_code}
     
-    result = Notes.update_one(query, {"$pull": {key: notes_to_delete[0]}})
+    result = Notes.delete_note(query, key, notes_to_delete)
     
     if result.modified_count > 0:
         return("Note deleted successfully.")
@@ -90,7 +111,8 @@ def delete_note():
 
 @app.get('/api/get_all_notes/<access_code>')
 def get_notes(access_code):
-    notes = mongo.db.notes.find_one({'access_code': access_code})
+    key = {'access_code': access_code}
+    notes = Notes.get_all_notes(key)
     if not notes:
         return jsonify({'error': 'Invalid access code'}), 404
     
